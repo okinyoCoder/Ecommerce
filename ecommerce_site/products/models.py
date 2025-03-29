421e1c4a35ed53facd1fcecfd10a92cfb9e45fb2
okinyoCoder <okinyoanthony@gmail.com>
2025-03-29 17:16:08 +0300

    updated model table

--- ecommerce_site/products/models.py
+++ ecommerce_site/products/models.py
@@ -2,22 +2,6 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractUser
 #Description, Price, Category, Stock Quantity, Image URL, and Created Date.
 
-class Product(models.Model):
-    class Categ(models.TextChoices):
-        Elec = 'lectronic'
-        Pers ='personal care'
-        Gro = 'grocery'
-        Foo ='food'
-        Clo ='clothing' 
-
-    name = models.TextField(max_length=100)
-    price = models.DecimalField(decimal_places=3)
-    stock = models.IntegerField()
-    image_url = models.ImageField(upload_to='profile_photos/')
-    category = models.CharField( max_length=3, choices=Categ, default=Categ.Foo)
-
-class OrderItem(models.model):
-    ...
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, password=None, **extra_fields):
         """
@@ -51,3 +35,35 @@ class CustomUser(AbstractUser):
     
     objects = CustomUserManager()
     USERNAME_FIELD = 'email'
+
+
+class Product(models.Model):
+    class Categ(models.TextChoices):
+        Elec = 'lectronic'
+        Pers ='personal care'
+        Gro = 'grocery'
+        Foo ='food'
+        Clo ='clothing' 
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=256, blank=False, null=False)
+    price = models.DecimalField(decimal_places=3)
+    stock = models.IntegerField()
+    image_url = models.ImageField(upload_to='profile_photos/')
+    category = models.CharField( max_length=3, choices=Categ, default=Categ.Foo)
+    created_date = models.DateField(auto_created=True)
+
+class OrderItem(models.model):
+    product_item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    cost = models.DecimalField(decimal_places=3)
+
+class Pay(models.model):
+    order = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    total_price = models.DecimalField(decimal_places=3) 
+    created_date = models.DateField(auto_created=True)
+    status = models.BooleanField(default=False)
+
+class Review(models.Model):
+    ...
